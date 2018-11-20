ba74f97cd8c1732eda1f1cbee14c5cc2b155d5d9
Anita Mohan <anitachandlermohan@gmail.com>
2018-11-20 16:03:12 +0200

    second commit

--- src/exercisePackage/Main.java
+++ src/exercisePackage/Main.java
@@ -4,33 +4,33 @@ public class Main {
 	
 		public static void main(String arg[]) {
 		helloWorld();
-		System.out.println(add(0,8,false));
+		add(0,7,false);
 
 		}
 		public static void helloWorld() {
 			System.out.println("Hello World!");
 		}
-		public static int add(int number1,int number2, boolean isAnitaHungry){
-			if (number1 == 0) {
-				return number2;
-			}
-			else if(number2 == 0) {
-				return number1;
-			}
+		public static int add(int number1,int number2,boolean isAnitaHungry){		
+		if( number1 == 0 ) {
 			
-			if (isAnitaHungry == true) {
+			return number2;
+	
+		}
+		else if (number2 == 0) {
 			
+			return number1;
+		}
+		else {
+			if(isAnitaHungry == true) {
 				int number3 = number1 + number2;
-				return number3;
 				
+				return number3;
 			}
 			else {
 				int number3 = number1 * number2;
 				return number3;
-			
 			}
-			
 		}
-		
+		}
 
 }
